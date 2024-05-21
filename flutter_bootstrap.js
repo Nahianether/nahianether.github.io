6648f28fac8c3a410030bfc193326c0e1083871d
Nahianether <nahianether3@gmail.com>
2024-05-22 01:17:58 +0600

    my portfolio commit4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4290107320"
+    serviceWorkerVersion: "641777359"
   }
 });
